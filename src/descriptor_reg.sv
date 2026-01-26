1698e61bf12fbb90689f25f1db0d293369a2ef31
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 23:11:30 +0530

    Update descriptor_reg.sv

--- src/descriptor_reg.sv
+++ src/descriptor_reg.sv
@@ -3,9 +3,29 @@ class descriptor_addr_reg extends uvm_reg;
   
   rand uvm_reg_field descriptor_addr;
   
+  covergroup descriptor_cov;
+    option.per_instance=1;
+    coverpoint descriptor_addr.value;
+  endgroup
+  
   function new(string name="");
-    super.new(name,32,UVM_NO_COVERAGE);
+    super.new(name,32,UVM_CVR_FIELD_VALS);
+    if(has_coverage(UVM_CVR_FIELD_VALS))
+      descriptor_cov=new();
   endfunction
+  
+  virtual function void sample(uvm_reg_data_t data,
+                               uvm_reg_data_t byte_en,
+                               bit is_read,
+                               uvm_reg_map map);
+    descriptor_cov.sample();
+  endfunction
+  
+  virtual function void sample_values();
+    super.sample_values();
+    descriptor_cov.sample();
+  endfunction
+  
   
   function void build();
     
