1fe152e248465570e76db79f8046f01283302033
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 12:27:28 +0530

    Refactor test classes and sequences in test.sv

--- src/test.sv
+++ src/test.sv
@@ -25,30 +25,56 @@ class mem_addr_test extends uvm_test;
   endtask
 endclass
 
-//intr reg seq
+//peak poke seq
 
-// class intr_test extends uvm_test;
-//   `uvm_component_utils(intr_test)
+class peak_poke_test extends uvm_test;
+  `uvm_component_utils(peak_poke_test)
 
-//   function new(string name  = "", uvm_component parent= null);
-//     super.new(name ,parent);
-//   endfunction
+  function new(string name  = "", uvm_component parent= null);
+    super.new(name ,parent);
+  endfunction
 
-//   environment env;
-//   intr_reg_seq seq2;
+  environment env;
+  peak_poke_seq seq2;
 
-//   virtual function void build_phase(uvm_phase phase);
-//     super.build_phase(phase);
-//     env   = environment::type_id::create("env", this);
-//     seq2  = intr_reg_seq::type_id::create("seq2");
-//   endfunction
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env   = environment::type_id::create("env", this);
+    seq2  = peak_poke_seq::type_id::create("seq2");
+  endfunction
 
-//   virtual task run_phase(uvm_phase phase);
-//     phase.raise_objection(this);
-//     seq2.reg_blk = env.regmodel;
-//     seq2.start(env.agt.seqr);
-//     phase.drop_objection(this);
-//     //phase.phase_done.set_drain_time(this, 200);
-//   endtask
-// endclass
+  virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq2.reg_blk = env.regmodel;
+    seq2.start(env.agt.seqr);
+    phase.drop_objection(this);
+    //phase.phase_done.set_drain_time(this, 200);
+  endtask
+endclass
 
+//regression test seq
+
+class regression_test extends uvm_test;
+  `uvm_component_utils(regression_test)
+
+  environment         env;
+  regression_seq  reg_seq;
+
+  function new(string name="", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    env     = environment::type_id::create("env", this);
+    reg_seq = regression_seq::type_id::create("reg_seq");
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    reg_seq.reg_blk = env.regmodel;
+    reg_seq.start(env.agt.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
