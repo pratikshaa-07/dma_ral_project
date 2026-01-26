0c22eb5cc6fd7e0dc53ffc78dfdc1660900791d2
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 12:23:23 +0530

    Update seq_item.sv

--- src/seq_item.sv
+++ src/seq_item.sv
@@ -1,1 +1,20 @@
+class dma_seq_item extends uvm_sequence_item;
+  rand bit [31:0] addr;
+  rand bit [31:0] wdata;
+  rand bit  wr_en;
+  rand bit  rd_en;
+   
+  bit [31:0] rdata;
 
+  `uvm_object_utils_begin(dma_seq_item)
+    `uvm_field_int(addr,UVM_ALL_ON)
+    `uvm_field_int(wdata,UVM_ALL_ON)
+    `uvm_field_int(rdata,UVM_ALL_ON)
+    `uvm_field_int(wr_en,UVM_ALL_ON)
+    `uvm_field_int(rd_en,UVM_ALL_ON)
+  `uvm_object_utils_end
+
+  function new(string name = "");
+    super.new(name);
+  endfunction
+endclass
