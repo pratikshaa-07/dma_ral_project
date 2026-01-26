25038456732383e30d8481f4772319abbf15555e
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 23:10:10 +0530

    Update status_reg.sv

--- src/status_reg.sv
+++ src/status_reg.sv
@@ -1,10 +1,6 @@
 class status_reg extends uvm_reg;
   `uvm_object_utils(status_reg)
   
-  function new(string name="");
-    super.new(name,32,UVM_NO_COVERAGE);
-  endfunction
-
   rand uvm_reg_field busy;
   rand uvm_reg_field done;
   rand uvm_reg_field error;
@@ -12,6 +8,33 @@ class status_reg extends uvm_reg;
   rand uvm_reg_field current_state;
   rand uvm_reg_field fifo_level;
   rand uvm_reg_field reserved;
+  
+  covergroup status_cov;
+    option.per_instance=1;
+    coverpoint busy.value; 
+    coverpoint done.value;
+    coverpoint error.value;
+    coverpoint paused.value;
+    coverpoint current_state.value;
+    coverpoint fifo_level.value;
+    coverpoint reserved.value;
+  endgroup
+  
+  function new(string name="");
+    super.new(name,32,UVM_CVR_FIELD_VALS);
+    if(has_coverage(UVM_CVR_FIELD_VALS))
+      status_cov=new();
+  endfunction
+  
+  
+  virtual function void sample(uvm_reg_data_t data,uvm_reg_data_t byte_en,bit is_read,uvm_reg_map map);
+    status_cov.sample();
+  endfunction
+  
+  virtual function void sample_values();
+    super.sample_values();
+    status_cov.sample();
+  endfunction
   
   function void build();
     busy = uvm_reg_field::type_id::create("busy");
