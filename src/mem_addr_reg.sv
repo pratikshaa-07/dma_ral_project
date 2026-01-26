b95cb79e9bfab3cbfac919ea1911c3556f55c97b
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 12:29:16 +0530

    Create mem_addr_reg class for memory address register
    Added a new UVM register class for memory address.

--- src/mem_addr_reg.sv
+++ src/mem_addr_reg.sv
@@ -1,1 +1,16 @@
-
+class mem_addr_reg extends uvm_reg;
+  `uvm_object_utils(mem_addr_reg)
+  
+  rand uvm_reg_field mem_addr;
+  
+  function new(string name="");
+    super.new(name,32,UVM_NO_COVERAGE);
+  endfunction
+  
+  function void build();
+    
+    mem_addr = uvm_reg_field::type_id::create("mem_addr");
+mem_addr.configure(.parent(this),.size(32),.lsb_pos(0),.access("RW"),.volatile(0),.reset('h0),.has_reset(1),.is_rand(1),.individually_accessible(1));  
+    
+  endfunction
+endclass
