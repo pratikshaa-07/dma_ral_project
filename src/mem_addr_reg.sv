157bfea7bbad33fb947b722ccae6ad7644ebb197
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 23:09:12 +0530

    Update mem_addr_reg.sv

--- src/mem_addr_reg.sv
+++ src/mem_addr_reg.sv
@@ -3,8 +3,24 @@ class mem_addr_reg extends uvm_reg;
   
   rand uvm_reg_field mem_addr;
   
+  covergroup mem_addr_cov;
+   option.per_instance=1;
+   coverpoint mem_addr.value; 
+  endgroup
+  
   function new(string name="");
-    super.new(name,32,UVM_NO_COVERAGE);
+    super.new(name,32,UVM_CVR_FIELD_VALS);
+    if(has_coverage(UVM_CVR_FIELD_VALS))
+      mem_addr_cov=new();
+  endfunction
+  
+  virtual function void sample(uvm_reg_data_t data,uvm_reg_data_t byte_en,bit is_read,uvm_reg_map map);
+    mem_addr_cov.sample();
+  endfunction
+  
+  virtual function void sample_values();
+    super.sample_values();
+    mem_addr_cov.sample();
   endfunction
   
   function void build();
