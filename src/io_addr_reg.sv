6cbc38001251dbe5718489f0460e83da48ed3884
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-01-26 23:08:47 +0530

    Update io_addr_reg.sv

--- src/io_addr_reg.sv
+++ src/io_addr_reg.sv
@@ -3,8 +3,28 @@ class io_addr_reg extends uvm_reg;
   
   rand uvm_reg_field io_addr;
   
+  covergroup io_addr_cov;
+    option.per_instance=1;
+    coverpoint io_addr.value; 
+  endgroup
+  
+    virtual function void sample(uvm_reg_data_t data,
+                               uvm_reg_data_t byte_en,
+                               bit is_read,
+                               uvm_reg_map map);
+    io_addr_cov.sample();
+  endfunction
+  
+    
   function new(string name="");
-    super.new(name,32,UVM_NO_COVERAGE);
+    super.new(name,32,UVM_CVR_FIELD_VALS);
+    if(has_coverage(UVM_CVR_FIELD_VALS))
+      io_addr_cov=new();
+  endfunction
+  
+  virtual function void sample_values();
+    super.sample_values();
+    io_addr_cov.sample();
   endfunction
   
   function void build();
